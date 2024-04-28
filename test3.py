5ed6fd427cc9c30dfe0ee03dfa2d27860fac8619
1136623363 <1136623363@qq.com>
2024-04-28 20:01:02 +0800

    xx

--- test3.py
+++ test3.py
@@ -4,17 +4,19 @@ from utils import *
 # 614417756: 5988526
 # silk_id=843278704
 # x_vayne=1315903
-silk_id=612528803
-x_vayne=2395933
-methodname = 'SilkwormService.UploadOrderVoucher'
-
-token = get_token(silk_id)
-data = get_data(methodname)
-data.update({"silk_id": silk_id})
-resp = requ(silk_id=silk_id, json_data=data, methodname=methodname, x_vayne=x_vayne,
-        servername=get_servername(methodname), token=token)
-
-print(resp)
+# silk_id=612528803
+# x_vayne=2395933
+silk_id = 843278704
+x_vayne = 1315903
+# methodname = 'SilkwormService.UploadOrderVoucher'
+#
+# token = get_token(silk_id)
+# data = get_data(methodname)
+# data.update({"silk_id": silk_id})
+# resp = requ(silk_id=silk_id, json_data=data, methodname=methodname, x_vayne=x_vayne,
+#         servername=get_servername(methodname), token=token)
+#
+# print(resp)
 # def __():
 #     methodname = 'SilkwormLotteryMobile.AddLotteryTimes'
 #
